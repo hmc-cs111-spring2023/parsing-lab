15826f71fb5597848530198d5e53ab7dfebc6b24
Ben Wiedermann <benw@cs.hmc.edu>
2023-03-01 11:19:32 -0800

    Working on tools

--- src/main/scala/calculator/calc.scala
+++ src/main/scala/calculator/calc.scala
@@ -1,7 +1,54 @@
-package calculator
+import scala.io.Source
+import Console.{RED, RESET}
+import util.control.Breaks._
 
-import tools.repl
+import calculator.parser._
+import calculator.ir._
+import calculator.semantics._
+
+/** Format an error message */
+def error(message: String): String =
+  s"${RESET}${RED}[Error] ${message}${RESET}"
+
+/** Evaluate an AST, but catch arithmetic exceptions */
+def doEval(ast: Expr): String =
+  try {
+    eval(ast).toString
+  } catch {
+    case e: ArithmeticException => error(e.getMessage)
+  }
+
+/** Parse a line and potentially evaluate it */
+def parseAndEvalLine(input: String) =
+  CalcParser(input) match
+    case CalcParser.Success(ast, _) => println(doEval(ast))
+    case e: CalcParser.NoSuccess    => println(error(e.toString))
+
+/** Parse a file and potentially evaluate it */
+def runFile(filename: String): Unit =
+  try {
+    val input = Source.fromFile(filename).mkString
+    parseAndEvalLine(input)
+  } catch {
+    case e: java.io.FileNotFoundException => println(error(e.getMessage))
+  }
+
+/** A simple REPL for the calculator */
+def repl(prompt: String = "> ") =
+  breakable {
+    try {
+      while (true) {
+        // Read input from user
+        print(prompt)
+        val input = scala.io.StdIn.readLine()
+        parseAndEvalLine(input)
+      }
+    } catch {
+      case e: Exception => break // end the loop
+    }
+  }
 
 @main
-def Calculator: Unit =
-  repl()
+def main(args: String*): Unit =
+  if args.isEmpty then repl()
+  else runFile(args(0))
